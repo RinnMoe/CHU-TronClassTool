53d41446c11835db18e172dada078b512ba5f717
Rinn <github@rinn.moe>
2025-11-13 22:48:05 +0800

    update 0.2.0

--- rollcall-bot_CHU/main.py
+++ rollcall-bot_CHU/main.py
@@ -1,11 +1,15 @@
-# CHU-TronClassTool 0.1.2
-# transplant by Rinn
+# CHU-TronClassTool
+ver = "0.2.0"
+
+# transplanted by Rinn
 # origin repository https://github.com/KrsMt-0113/XMU-Rollcall-Bot
+
 import datetime
 import time
 import json
-import uuid
+# import uuid
 import requests
+from rich import print
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.ie.service import Service
@@ -14,91 +18,135 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from parse_rollcalls import parse_rollcalls
 
-# 读取学号、密码、轮询间隔
-with open("config.json", encoding='utf-8') as f:
-    config = json.load(f)
-    username = config["username"]
-    password = config["password"]
-    interval = config["interval"]
-    longitude = config['longitude']
-    latitude = config['latitude']
+def main():
+    # 读取学号、密码、轮询间隔、经纬度
+    try:
+        with open("config.json", encoding='utf-8') as f:
+            config = json.load(f)
+            username = config["username"]
+            password = config["password"]
+            url = config["url"]
+            interval = config["interval"]
+            longitude = config["longitude"]
+            latitude = config["latitude"]
+            driver = config["driver"]
+            run_headless = config["run_headless"]
 
-# 签到列表获取接口，轮询间隔，轮询脚本
-api_url = "https://course-online.chd.edu.cn/api/radar/rollcalls"
-fetch_script = """
-const url = arguments[0];
-const callback = arguments[arguments.length - 1];
-fetch(url, {credentials: 'include'})
-  .then(resp => resp.text().then(text => callback({status: resp.status, ok: resp.ok, text: text})))
-  .catch(err => callback({error: String(err)}));
-"""
+    except (json.JSONDecodeError, FileNotFoundError):
+        print("初次运行或配置文件损坏，正在生成配置文件 config.json，请填写相关信息后重新运行程序。")
+        username = input('请输入您的学号: ')
+        password = input('请输入您的密码: ')
+        url = input('请输入 TronClass 平台地址 (默认 https://course-online.chd.edu.cn): ') or "https://course-online.chd.edu.cn"
+        interval = input('请输入轮询间隔，单位秒 (默认 5): ') or 5
+        longitude = input('请输入签到经度 (默认 113.000000): ') or 113.
+        latitude = input('请输入签到纬度 (默认 28.000000): ') or 28.
+        driver = input('请输入 ChromeDriver 路径 (默认 chromedriver.exe): ') or 'chromedriver.exe'
+        run_headless = input('是否无头运行？(y/n, 默认 y): ') or 'y'
+        config = {
+            "username": username,
+            "password": password,
+            "url": url,
+            "interval": int(interval),
+            "longitude": float(longitude),
+            "latitude": float(latitude),
+            "driver": driver,
+            "run_headless": True if run_headless.lower() == 'y' else False
+        }
+        with open("config.json", "w", encoding='utf-8') as f:
+            json.dump(config, f, ensure_ascii=False, indent=4)
 
-chrome_options = Options()
-# chrome_options.add_argument("--start-maximized")   # 有头调试
-chrome_options.add_argument("--headless")  # 无头运行
+    # 签到列表获取接口，轮询间隔，轮询脚本
+    api_url = f"{url}/api/radar/rollcalls"
+    fetch_script = """
+    const url = arguments[0];
+    const callback = arguments[arguments.length - 1];
+    fetch(url, {credentials: 'include'})
+      .then(resp => resp.text().then(text => callback({status: resp.status, ok: resp.ok, text: text})))
+      .catch(err => callback({error: String(err)}));
+    """
 
-# 启动selenium
-print(f"CHU-TronClassTool 0.1.2\ntransplant by Rinn\n正在初始化...")
-try:
-    driver = webdriver.Chrome(chrome_options, service=Service('chromedriver.exe'))
-except ValueError as e:
-    print(f"找不到浏览器文件。\n{e}")
+    chrome_options = Options()
 
-driver.get("https://course-online.chd.edu.cn/user/index#/")
-print("已连接 TronClass\n登录中...")
+    if run_headless:
+        chrome_options.add_argument("--headless")  # 无头运行
 
-WebDriverWait(driver, 10, 0.5).until(EC.title_contains('统一身份认证平台'))
-driver.find_element(By.ID, "username").send_keys(username)
-driver.find_element(By.ID, "password").send_keys(password)
-driver.find_element(By.ID, "login_submit").click()
+    # 启动selenium
+    print(f"CHU-TronClassTool {ver}\ntransplanted by Rinn\n正在初始化...")
+    print(f"=================")
+    print(f"当前配置: \n平台地址: {url} \n轮询间隔: {interval}秒")
+    try:
+        driver = webdriver.Chrome(chrome_options, service=Service("chromedriver.exe"))
+    except ValueError as e:
+        print(f"找不到浏览器文件。\n{e}")
+        #todo: 下载 chromedriver
+        exit()
 
-WebDriverWait(driver, 100, 0.5).until(EC.title_contains('首页 - 畅课'))
-print(f'用户 {driver.find_element(By.ID, "userCurrentName").text} 登录成功')
+    driver.get(f"{url}/user/index#/")
+    print("已连接 TronClass\n登录中...")
 
-res = requests.get(api_url, cookies={c['name']: c['value'] for c in driver.get_cookies()})
-if res.status_code == 200:
-    print("五秒后进入监测...")
-else:
-    print("登录失败。五秒后程序退出。")
-    driver.quit()
+    WebDriverWait(driver, 10, 0.5).until(EC.title_contains('统一身份认证平台'))
+    driver.find_element(By.ID, "username").send_keys(username)
+    driver.find_element(By.ID, "password").send_keys(password)
+    driver.find_element(By.ID, "login_submit").click()
+
+    WebDriverWait(driver, 100, 0.5).until(EC.title_contains('首页 - 畅课'))
+    print(f'用户 {driver.find_element(By.ID, "userCurrentName").text} 登录成功')
+
+    res = requests.get(api_url, cookies={c['name']: c['value'] for c in driver.get_cookies()})
+    if res.status_code == 200:
+        print("五秒后进入监测...")
+    else:
+        print("登录失败。五秒后程序退出。")
+        driver.quit()
+        time.sleep(5)
+        exit(0)
+
     time.sleep(5)
-    exit(0)
 
-time.sleep(5)
+    print(f"签到监测启动。")
 
-deviceID = uuid.uuid4()
-print(f"签到监测启动。")
+    temp_data = {'rollcalls': []}
+    check_count = 0
 
-start = time.time()
-temp_data = {'rollcalls': []}
-check_count = 0
-
-while True:
-    res = driver.execute_async_script(fetch_script, api_url)
-
-    check_count += 1
-
-    if check_count % 5 == 0:
-        print(f'[{datetime.datetime.now().strftime("%H:%M:%S")}] 监测已进行了{check_count}次')
-
-    if res['status'] == 200:
-        text = res.get('text', '')
+    while True:
         try:
-            data = json.loads(text)
-            if temp_data == data:
-                time.sleep(interval)
-                continue
-            else:
-                temp_data = data
-                if len(temp_data['rollcalls']) > 0:
-                    if not parse_rollcalls(temp_data, driver, longitude, latitude):
-                        temp_data = {'rollcalls': []}
+            res = driver.execute_async_script(fetch_script, api_url)
         except Exception as e:
-            print(time.strftime("%H:%M:%S", time.localtime()), ":发生错误")
+            print(f'[{datetime.datetime.now().strftime("%H:%M:%S")}] 发生了错误-{e}')
+            break
 
-    elif res['status'] != 200:
-        print("失去连接，请重新登录。")
-        break
-    time.sleep(interval)
+        check_count += 1
 
-driver.quit()
+        if check_count % 5 == 0:
+            print(f'[{datetime.datetime.now().strftime("%H:%M:%S")}] 监测已进行了{check_count}次')
+
+        if res['status'] == 200:
+            text = res.get('text', '')
+            try:
+                data = json.loads(text)
+                if temp_data == data:
+                    time.sleep(interval)
+                    continue
+                else:
+                    temp_data = data
+                    if len(temp_data['rollcalls']) > 0:
+                        if not parse_rollcalls(temp_data, driver, longitude, latitude):
+                            temp_data = {'rollcalls': []}
+            except Exception as e:
+                print(f"[{time.strftime("%H:%M:%S", time.localtime())}]:发生错误 -{e}")
+
+        elif res['status'] != 200:
+            print("失去连接，请重新登录。")
+            break
+        time.sleep(interval)
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("用户中断，程序退出。")
+    except Exception as e:
+        print(f"发生错误，程序退出。\n{e}")
--- rollcall-bot_CHU/parse_rollcalls.py
+++ rollcall-bot_CHU/parse_rollcalls.py
@@ -13,12 +13,11 @@ def decode_rollcall(data):
                     'created_by_name': rollcall['created_by_name'],
                     'department_name': rollcall['department_name'],
                     'is_expired': rollcall['is_expired'],
-                    'is_number': rollcall['is_number'],
-                    'is_radar': rollcall['is_radar'],
                     'rollcall_id': rollcall['rollcall_id'],
                     'rollcall_status': rollcall['rollcall_status'],
                     'scored': rollcall['scored'],
-                    'status': rollcall['status']
+                    'status': rollcall['status'],
+                    'source': rollcall['source']
                 }
             )
     else:
@@ -29,16 +28,20 @@ def parse_rollcalls(data, driver, longitude, latitude):
     undone = 0
     count, rollcalls = decode_rollcall(data)
     if count:
-        print(time.strftime("%H:%M:%S", time.localtime()),f"监测到新的签到活动。\n")
+        print(f"[{time.strftime("%H:%M:%S", time.localtime())}] 发现 {count} 个签到活动。")
         for i in range(count):
+            #Todo：Bugs here need to be fixed
             print(f"第 {i+1} 个，共 {count} 个：")
             print(f"课程名称：{rollcalls[i]['course_title']}")
-            print(f"签到创建：{rollcalls[i]['created_by_name']}")
-            print(f"签到状态：{rollcalls[i]['rollcall_status']}")
-            print(f"是否计分：{rollcalls[i]['scored']}")
-            print(f"出勤情况：{rollcalls[i]['status']}")
-            if not rollcalls[i]['status'] == 'absent':
-                undone = 0
+            print(f"创建者：{rollcalls[i]['created_by_name']}")
+            print(f"状态：{'进行中' if rollcalls[i]['rollcall_status'] == 'in_progress' else '已签到'}")
+            print(f"是否计分：{'是' if rollcalls[i]['scored'] else '否'} ")
+            if rollcalls[i]['status'] == 'absent':
+                undone = 1
+                print("出勤情况：未签到")
+            else:
+                print("出勤情况：已签到")
+
             if rollcalls[i]['source'] == "number":
                 temp_str = "数字签到"
             elif rollcalls[i]['source'] == "qr":
@@ -59,6 +62,7 @@ def parse_rollcalls(data, driver, longitude, latitude):
                 print(f'未能识别该签到类型-{rollcalls[i]['source']}')
             print(f"签到类型：{temp_str}\n")
             if undone:
+                print(f"[{time.time()}] 开始应答第 {i+1} 个签到...")
                 if temp_str == "数字签到":
                     if send_code(driver, rollcalls[i]['rollcall_id']):
                         print("数字签到成功！")
